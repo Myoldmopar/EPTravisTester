93665422621504bdf281c4e5c6c1ad51339c659e
Edwin Lee <leeed2001@gmail.com>
2023-03-29 08:41:16 -0500

    Bump to 23.1

--- ep_testing/config.py
+++ ep_testing/config.py
@@ -47,6 +47,6 @@ class TestConfiguration:
         self.bitness = this_config['bitness']
 
         self.this_version = '23.1'
-        self.tag_this_version = 'v23.1.0-RC3'
+        self.tag_this_version = 'v23.1.0'
         self.last_version = '22.2'
         self.tag_last_version = 'v22.2.0'
