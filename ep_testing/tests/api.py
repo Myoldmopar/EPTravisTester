2472487c91fe38f2e7e1119d26b8711dc9f1b48b
Edwin Lee <leeed2001@gmail.com>
2024-03-15 07:37:37 -0500

    Go back to non-plugin API test on Mac GHA

--- ep_testing/tests/api.py
+++ ep_testing/tests/api.py
@@ -77,8 +77,7 @@ class TestPythonAPIAccess(BaseTest):
                 my_env["PATH"] = install_root + ";" + my_env["PATH"]
             if self.os == OS.Mac:
                 # while it runs OK locally, for some reason on GHA, running a Plugin file from the Python API seg-faults
-                idf_to_run = os.path.join(install_root, 'ExampleFiles', 'PythonPluginCustomOutputVariable.idf')
-                # '1ZoneUncontrolled.idf')
+                idf_to_run = os.path.join(install_root, 'ExampleFiles', '1ZoneUncontrolled.idf')
             else:
                 idf_to_run = os.path.join(install_root, 'ExampleFiles', 'PythonPluginCustomOutputVariable.idf')
             my_check_call(self.verbose, [py, python_file_path, '-D', idf_to_run], env=my_env)
